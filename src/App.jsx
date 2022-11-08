0d3ad520db943d8ec7cf9e89c27de3b3362d55ae
Jason Dansie <jasondansie@gmail.com>
2022-11-08 10:39:02 +0200

    cleans app and starts the framework

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  state = {
+    firstname: "",
+    lastname: "",
+  };
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default App;
---
+++ src/form.jsx
@@ -0,0 +1,12 @@
+import './form.css';
+
+const Form = (props) => {
+    return (
+        <form className='form'>
+            <div>
+                <label htmlFor="firstName"></label>
+            </div>
+
+        </form>
+    );
+}
